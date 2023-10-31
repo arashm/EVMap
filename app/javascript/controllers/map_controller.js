a7cda0e9e1e253a4ae1b85b76f957402b789bec7
Arash Mousavi <me@amousavi.dev>
2023-10-31 21:07:52 +0000

    Add offline support for loading map tiles

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from '@hotwired/stimulus'
 import L from 'leaflet'
+import { getStorageLength, getBlobByKey, downloadTile, saveTile } from 'leaflet.offline'
 import Rails from '@rails/ujs'
 import { RED_ICON_URL } from 'constants/assets'
 
@@ -8,12 +9,13 @@ const MAP_BASE_URL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
 // Connects to data-controller="map"
 export default class extends Controller {
   static targets = ['mapContainer', 'searchInput']
-  static lat = 52.52000
-  static lng = 13.404954
+  static initial_lat = 52.52000
+  static initial_lng = 13.404954
 
   connect () {
     this.initMap()
     this.fetchChargers()
+    getStorageLength().then(i => console.log(i + ' tiles in storage'))
   }
 
   search (event) {
@@ -48,12 +50,44 @@ export default class extends Controller {
   get map () {
     if (this.mapObject) return this.mapObject
 
-    this.mapObject = L.map(this.mapContainerTarget).setView([this.constructor.lat, this.constructor.lng], 13)
+    this.mapObject = L.map(this.mapContainerTarget).setView([this.constructor.initial_lat, this.constructor.initial_lng], 13)
     return this.map
   }
 
   initMap () {
-    L.tileLayer(MAP_BASE_URL).addTo(this.map)
+    const baseLayer = L.tileLayer(MAP_BASE_URL, { minZoom: 13 })
+
+    baseLayer.on('tileloadstart', (event) => {
+      const { tile } = event
+      const url = tile.src
+      // reset tile.src, to not start download yet
+      tile.src = ''
+      getBlobByKey(url).then((blob) => {
+        if (blob) {
+          tile.src = URL.createObjectURL(blob)
+          console.debug(`Loaded ${url} from idb`)
+          return
+        }
+        tile.src = url
+        // create helper function for it?
+        const { x, y, z } = event.coords
+        const { _url: urlTemplate } = event.target
+        const tileInfo = {
+          key: url,
+          url,
+          x,
+          y,
+          z,
+          urlTemplate,
+          createdAt: Date.now()
+        }
+        console.log(tileInfo)
+        downloadTile(url)
+          .then((dl) => saveTile(tileInfo, dl))
+          .then(() => console.debug(`Saved ${url} in idb`))
+      })
+    })
+    baseLayer.addTo(this.map)
   }
 
   fetchChargers () {
