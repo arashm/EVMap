17258ea2cfd95a9fea8ffd2620a8f53f94f220be
Arash Mousavi <me@amousavi.dev>
2023-11-01 15:56:42 +0000

    Implement chaching for routes in service workers

--- app/views/service_worker/service_worker.js
+++ app/views/service_worker/service_worker.js
@@ -1,3 +1,8 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js')
+
+const { CacheFirst, NetworkFirst } = workbox.strategies
+const { registerRoute } = workbox.routing
+
 function onInstall (event) {
   console.log('[Serviceworker]', 'Installing!', event)
 }
@@ -13,3 +18,8 @@ function onFetch (event) {
 self.addEventListener('install', onInstall)
 self.addEventListener('activate', onActivate)
 self.addEventListener('fetch', onFetch)
+
+registerRoute(({ url }) => url.pathname.startsWith('/'), new NetworkFirst({ cacheName: 'document' }))
+registerRoute(({ url }) => url.pathname.startsWith('/charger'), new NetworkFirst({ cacheName: 'chargers' }))
+registerRoute(({ request }) => request.destination === 'script' || request.destination === 'style', new CacheFirst({ cacheName: 'assets-styles-and-scripts' }))
+registerRoute(({ request }) => request.destination === 'image', new CacheFirst({ cacheName: 'assets-images' }))
